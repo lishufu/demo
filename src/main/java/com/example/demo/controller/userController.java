7d97eeb5cf6f80432613e9adaa1491be5dd1538f
lishufu <sfli@jmgo.com>
2019-10-15 15:42:31 +0800

    test conn

--- src/main/java/com/example/demo/controller/userController.java
+++ src/main/java/com/example/demo/controller/userController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class userController {
     @RequestMapping("/")
     public String test() {
-        return "hello word";
+        return "hello word1";
     }
 }
